b2ddb9694ccb5581c8acbdcf1884de187d1b3a54
Nurmuhammad <rahmatullayevnurmuhammad9@gmail.com>
2023-12-29 17:28:24 +0500

    server

--- src/assets/Components/Header.jsx
+++ src/assets/Components/Header.jsx
@@ -4,10 +4,12 @@ const Header = () => {
     return (
         <header className='bg-slate-200 shadow-md'>
             <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
-                <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-                    <span className='text-slate-500'>Sahand</span>
-                    <span className='text-slate-700'>Estate</span>
-                </h1>
+                <Link to='/'>
+                    <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
+                        <span className='text-slate-500'>Sahand</span>
+                        <span className='text-slate-700'>Estate</span>
+                    </h1>
+                </Link>
                 <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
                     <input
                         type="text"
@@ -18,13 +20,13 @@ const Header = () => {
                 </form>
                 <ul className="flex gap-4">
                     <Link to='/'>
-                    <li className="hidden sm:inline text-slate-700 hover:underline">Home</li>
+                        <li className="hidden sm:inline text-slate-700 hover:underline">Home</li>
                     </Link>
                     <Link to='/about'>
-                    <li className="hidden sm:inline text-slate-700 hover:underline">About</li>
+                        <li className="hidden sm:inline text-slate-700 hover:underline">About</li>
                     </Link>
                     <Link to='/sign-in'>
-                    <li className="text-slate-700 hover:underline">Sign In</li>
+                        <li className="text-slate-700 hover:underline">Sign In</li>
                     </Link>
                 </ul>
             </div>
